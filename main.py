dc857ff842d10c758d1b65f53d18fa38e6b46c2f
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2021-12-29 19:59:11 +0530

    Update main.py
    updation 29 december

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
+import plotly.graph_objects as go
 #********* Data Cleaning **************************
 filename = 'IBM ICE- Consolidated Student Count.xlsx'
 main_data = pd.read_excel(filename)
@@ -21,6 +22,7 @@ def students_per_course(data):
     fig = px.pie(count_df,values='no of students',names='course',title='Course Wise Strength')
     fig.update_traces(textposition = 'inside', textinfo='percent+label')
     return fig
+
 def students_per_batch(data):
     hist = data.groupby(['Batch','University']).sum().sum(1).unstack('University').sum(1)
     if any(hist.values):
@@ -35,6 +37,7 @@ def students_per_batch(data):
     xaxis_title="",
     yaxis_title="") 
     return fig
+
 def course_trend(data,course_list):
     g_df = data.groupby(['Batch']).sum()
     g_df['Batch'] = g_df.index
@@ -43,6 +46,29 @@ def course_trend(data,course_list):
     check_course = lambda x:any([x == e for e in course_list])
     result = result[result.Course.map(check_course)]
     fig = px.line(result,x='Batch',y='Strength',color='Course',text='Course',title='Course Trends')
+    return fig
+
+def batch_hist(data,year=None):
+    g_df = data.groupby(['Batch']).sum()
+    g_df['Batch'] = g_df.index
+    result = pd.melt(g_df,id_vars='Batch',value_name='count')
+    result.columns = ['Batch','Course','Strength']
+    if year:
+        result = result[result.Batch==year]
+        
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=result.Strength,
+        y=result.Course,
+        marker=dict(
+            color='rgba(50, 171, 96, 0.6)',
+            line=dict(
+                color='rgba(50, 171, 96, 1.0)',
+                width=1),),
+        name='current running courses',
+        orientation='h',
+    ))
+    fig.update_layout(title='Course Trends')
     return fig
 #******************* Main **********************
 
@@ -77,16 +103,26 @@ if selected_uni != 'All':
             selections.append(st.checkbox(course))
     selections = np.array(selections)    
     st.write(course_trend(data,courses[selections]))
+    #**************
+    batches = np.array(['All']+list(data.Batch.unique()))
+    year = st.selectbox(label='select a batch',options=batches)
+    if year!='All':
+        figure = batch_hist(data,year)
+        st.write(figure)
+    else:
+        figure = batch_hist(data)
+        st.write(figure)
 else:
     st.header('Overall Analysis')
-    total_students = main_data.iloc[:,2:].values.sum()
+    data = main_data
+    total_students = data.iloc[:,2:].values.sum()
     st.write('Total Strength : ',total_students)
-    st.write(students_per_course(main_data))
-    st.write(students_per_batch(main_data))
+    st.write(students_per_course(data))
+    st.write(students_per_batch(data))
 
     st.write('tick the courses')
     selections = []
-    courses = np.array(main_data.columns[2:])
+    courses = np.array(data.columns[2:])
     c1,c2,c3,c4 = st.columns(4)
     
     with c1:
@@ -102,4 +138,13 @@ else:
         for course in courses[12:16]:
             selections.append(st.checkbox(course))
     selections = np.array(selections)    
-    st.write(course_trend(main_data,courses[selections]))
+    st.write(course_trend(data,courses[selections]))
+    #****************
+    batches = np.array(['All']+list(data.Batch.unique()))
+    year = st.selectbox(label='select a batch',options=batches)
+    if year!='All':
+        figure = batch_hist(data,year)
+        st.write(figure)
+    else:
+        figure = batch_hist(data)
+        st.write(figure)
