dad2fae116f33b839dc0a810051ab42c46a9bf2d
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2022-01-12 01:50:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import numpy as np
+# import numpy as np
 import plotly.graph_objects as go
 #********* Data Cleaning **************************
 filename = 'IBM ICE- Consolidated Student Count.xlsx'
